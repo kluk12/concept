6fb5cbb43a4f4150727d7aa0e69843cd8a258d39
kluk12 <szymekkluk@interia.pl>
2019-03-25 11:11:06 +0100

    działa

--- src/App.js
+++ src/App.js
@@ -2,36 +2,73 @@ import React, { Component, useMemo } from "react";
 import * as THREE from "three";
 import { Canvas, useRender, useThree } from "react-three-fiber";
 import "./App.css";
-import Stl from "./../node_modules/three/examples/js/loaders/STLLoader";
+import { BoxGeometry } from "three";
+import { Thing2 } from "./componets/thing";
+
 class App extends Component {
   render() {
-    function Image({ url }) {
-      var loader = new THREE.STLLoader();
-      loader.load("./models/stl/ascii/slotted_disk.stl", function(geometry) {
-        var material = new THREE.MeshPhongMaterial({
-          color: 0xff5533,
-          specular: 0x111111,
-          shininess: 200
-        });
-        var mesh = new THREE.Mesh(geometry, material);
-        mesh.position.set(0, -0.25, 0.6);
-        mesh.rotation.set(0, -Math.PI / 2, 0);
-        mesh.scale.set(0.5, 0.5, 0.5);
-        mesh.castShadow = true;
-        mesh.receiveShadow = true;
-        // scene.add( mesh );
-      });
-      // const texture = useMemo(() => new Stl.STLLoader().load(url), [url]);
-      // return (
-      //   <mesh>
-      //     <planeBufferGeometry name="geometry" args={[1, 1]} />
-      //     <meshLambertMaterial name="material" transparent>
-      //       <primitive name="map" object={texture} />
-      //     </meshLambertMaterial>
-      //   </mesh>
-      // );
+    function Thing({ vertices, color }) {
+      return (
+        <group>
+          {/* <BoxGeometry>
+            <geometry
+              name="geometry"
+              vertices={new THREE.BoxGeometry(1, 1, 1)}
+              onUpdate={self => (self.verticesNeedUpdate = true)}
+            />
+            <lineBasicMaterial name="material" color="black" />
+          </BoxGeometry> */}
+          <mesh
+            // visible
+            // userData={{ test: "hello" }}
+            // position={new THREE.Vector3(1, 2, 3)}
+            // rotation={new THREE.Euler(0, 0, 0)}
+            geometry={new THREE.BoxGeometry(1, 1, 1)}
+            material={
+              new THREE.MeshBasicMaterial({
+                color: new THREE.Color("indianred")
+              })
+            }
+            onClick={e => {
+              console.log("cl");
+              return Thing2();
+            }}
+            onHover={e => console.log("hover")}
+            onUnhover={e => console.log("unhover")}
+          >
+            {/* <octahedronGeometry name="geometry" /> */}
+            {/* <meshBasicMaterial
+              name="material"
+              color="0x00ff00"
+              // opacity={0.5}
+              transparent
+            /> */}
+          </mesh>
+        </group>
+      );
     }
-    return <div className="App">{Image("./componets/stl/box.stl")}</div>;
+    // function App() {
+    //   // gl is the webgl-renderer
+    //   // canvas the dom element that was created
+    //   // size the bounds of the view (which stretches 100% and auto-adjusts)
+    //   // viewport is the calculated screen-size, it's a function
+    //   const { gl, canvas, scene, camera, size, viewport } = useThree();
+    //   // Subscribes to the render-loop, gets cleaned up automatically when the component unmounts
+    //   // Add a "true" as the 2nd argument and you take over the render-loop
+    //   useRender(({ gl, canvas, scene, camera }) =>
+    //     console.log("i'm in the render-loop")
+    //   );
+    //   return <group />;
+    // }
+
+    return (
+      // <App />
+      <Canvas>
+        <Thing
+        // vertices={[[-1, 0, 0], [0, 1, 0], [1, 0, 0], [0, -1, 0], [-1, 0, 0]]}
+        />
+      </Canvas>
+    );
   }
 }
 
--- src/componets/thing.js
+++ src/componets/thing.js
@@ -3,29 +3,42 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Canvas } from "react-three-fiber";
 
-function Thing({ vertices, color }) {
+export function Thing2() {
+  // {
+  //   vertices, color;
+  // }
   return (
-    <group ref={ref => console.log("we have access to the instance")}>
-      <line>
+    <group>
+      {/* <BoxGeometry>
         <geometry
           name="geometry"
-          vertices={vertices.map(v => new THREE.Vector3(...v))}
+          vertices={new THREE.BoxGeometry(1, 1, 1)}
           onUpdate={self => (self.verticesNeedUpdate = true)}
         />
         <lineBasicMaterial name="material" color="black" />
-      </line>
+      </BoxGeometry> */}
       <mesh
+        // visible
+        // userData={{ test: "hello" }}
+        position={new THREE.Vector3(1, 2, 3)}
+        // rotation={new THREE.Euler(0, 0, 0)}
+        geometry={new THREE.BoxGeometry(1, 1, 1)}
+        material={
+          new THREE.MeshBasicMaterial({
+            color: new THREE.Color("indianred")
+          })
+        }
         onClick={e => console.log("click")}
         onHover={e => console.log("hover")}
         onUnhover={e => console.log("unhover")}
       >
-        <octahedronGeometry name="geometry" />
-        <meshBasicMaterial
+        {/* <octahedronGeometry name="geometry" /> */}
+        {/* <meshBasicMaterial
           name="material"
-          color="peachpuff"
-          opacity={0.5}
+          color="0x00ff00"
+          // opacity={0.5}
           transparent
-        />
+        /> */}
       </mesh>
     </group>
   );
